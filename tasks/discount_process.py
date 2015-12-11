45308faf40bb18a7297be0e67c2e20c0f94671bd
Dennis <271393@qq.com>
2015-12-12 02:57:33 +0800

    minor fixes

--- tasks/discount_process.py
+++ tasks/discount_process.py
@@ -8,14 +8,13 @@ from indexer import rotate_index
 
 # pipeline
 func_list = [
-    base_price.calculate_min_max_price,     # 90 secs
-    promo_item_trans.processItemPromo,      # 90 secs
-    promo_item_trans.process_promo_detail,  # 10 secs
-    promo_item_trans.process_gift_value,    # 2 secs
-    worthy_mix.generate_worthy_mix_main,    # 300 secs
-    rotate_index.execute_rotate_index,    # 5 secs
-    rotate_index.flush_memcache_content,  # 1 secs
-    rotate_index.re_cache,                # ? secs
+    base_price.calculate_min_max_price,     # 150 secs
+    promo_item_trans.processItemPromo,      # 15 secs
+    promo_item_trans.process_promo_detail,  # 25 secs
+    promo_item_trans.process_gift_value,    # 1 secs
+    worthy_mix.generate_worthy_mix_main,    # 400 secs
+    rotate_index.execute_rotate_index,      # 10 secs
+    rotate_index.re_cache,                  # 10 secs
 ]
 
 if __name__ == '__main__':
