ad80e48e7cc9ad376df6356142fe9e59492b59f6
Dennis <271393@qq.com>
2015-12-13 01:31:59 +0800

    adding weighted ranking for search

--- worthy_service/sku_index_access.py
+++ worthy_service/sku_index_access.py
@@ -6,12 +6,23 @@ from datasys import data_config
 INDEX_NAME = 'worthy_index_main'
 INDEX_MAX_RECALL = 2500
 
+INDEX_WEIGHT_DICT = {
+    'title': 7,
+    'category_name':10,
+    'catalog_name':4,
+    'name_deduction':10,
+    'name_discount':10,
+    'name_lowest':10,
+    'property_text':4,
+    'category_text':4,
+}
 
 def getSearchResult(query):
     #### SPHINX CLIENT SETUP #####
     cl = SphinxClient()
     cl._host = data_config.SPHINX_HOST
-    cl.SetSortMode(SPH_SORT_EXTENDED,clause='rank_score DESC')
+    cl.SetIndexWeights(INDEX_WEIGHT_DICT)
+    cl.SetSortMode(SPH_SORT_EXPR,clause='@weight*rank_score')
     ##############################
 
     cl.SetLimits(0,INDEX_MAX_RECALL, 0)
@@ -28,7 +39,7 @@ def getSearchResult(query):
 
 if __name__ == "__main__":
 
-    ret = getSearchResult('衣')
+    ret = getSearchResult('键盘')
     print ret
     print len(ret)
 
