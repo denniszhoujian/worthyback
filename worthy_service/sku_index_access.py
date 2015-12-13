034d114c29e166558ab1e0e1eb7a8bda541f85cc
Dennis <271393@qq.com>
2015-12-13 12:37:04 +0800

    minor fixes

--- worthy_service/sku_index_access.py
+++ worthy_service/sku_index_access.py
@@ -2,30 +2,19 @@
 
 from worthy_service.sphinxapi import *
 from datasys import data_config
+import service_config
 
 INDEX_NAME = 'worthy_index_main'
-INDEX_MAX_RECALL = 2500
-
-INDEX_WEIGHT_DICT = {
-    'title': 7,
-    'category_name':10,
-    'catalog_name':4,
-    'name_deduction':10,
-    'name_discount':10,
-    'name_lowest':10,
-    'property_text':4,
-    'category_text':4,
-}
 
 def getSearchResult(query):
     #### SPHINX CLIENT SETUP #####
     cl = SphinxClient()
     cl._host = data_config.SPHINX_HOST
-    cl.SetIndexWeights(INDEX_WEIGHT_DICT)
+    cl.SetIndexWeights(service_config.SKU_INDEX_WEIGHT_DICT)
     cl.SetSortMode(SPH_SORT_EXPR,clause='@weight*rank_score')
     ##############################
 
-    cl.SetLimits(0,INDEX_MAX_RECALL, 0)
+    cl.SetLimits(0,service_config.SKU_LIST_MAX_RECALL_NUM, 0)
     ret = cl.Query(query)
 
     idlist = []
